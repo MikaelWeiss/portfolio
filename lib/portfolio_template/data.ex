f6fc9e0fa1fad2cbe16f702ec786f6473faf0184
Mikael Weiss <campingmikael@icloud.com>
2025-01-11 13:59:52 -0700

    Added my own data

--- lib/portfolio_template/data.ex
+++ lib/portfolio_template/data.ex
@@ -1,20 +1,23 @@
 defmodule PortfolioTemplate.Data do
-  @portfolio_data "priv/static/data/portfolio_data.json"
-                  |> File.read!()
-                  |> Jason.decode!(keys: :atoms)
+  @portfolio_data_path "priv/static/data/portfolio_data.json"
 
-  def get_all, do: @portfolio_data
-  def get_personal, do: @portfolio_data[:personal]
-  def get_about, do: @portfolio_data[:about]
-  def get_skills, do: @portfolio_data[:skills]
-  def get_experience, do: @portfolio_data[:experience]
-  def get_projects, do: @portfolio_data[:projects]
-  def get_awards, do: @portfolio_data[:awards]
-  def get_volunteer, do: @portfolio_data[:volunteer]
-  def get_education, do: @portfolio_data[:education]
+  def get_all do
+    @portfolio_data_path
+    |> File.read!()
+    |> Jason.decode!(keys: :atoms)
+  end
 
-  def get_featured_projects, do: @portfolio_data[:projects][:featured]
+  def get_personal, do: get_all()[:personal]
+  def get_about, do: get_all()[:about]
+  def get_skills, do: get_all()[:skills]
+  def get_experience, do: get_all()[:experience]
+  def get_projects, do: get_all()[:projects]
+  def get_awards, do: get_all()[:awards]
+  def get_volunteer, do: get_all()[:volunteer]
+  def get_education, do: get_all()[:education]
+
+  def get_featured_projects, do: get_all()[:projects][:featured]
 
   def get_all_projects,
-    do: @portfolio_data[:projects][:featured] ++ @portfolio_data[:projects][:all]
+    do: get_all()[:projects][:featured] ++ get_all()[:projects][:all]
 end
