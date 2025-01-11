eafebde6071e493cc6bd12b8ad01e886642e69b9
Mikael Weiss <campingmikael@icloud.com>
2025-01-11 12:46:52 -0700

    Got the website running again

--- lib/portfolio_template/data.ex
+++ lib/portfolio_template/data.ex
@@ -1,20 +1,20 @@
 defmodule PortfolioTemplate.Data do
   @portfolio_data "priv/static/data/portfolio_data.json"
                   |> File.read!()
-                  |> Jason.decode!()
+                  |> Jason.decode!(keys: :atoms)
 
   def get_all, do: @portfolio_data
-  def get_personal, do: @portfolio_data["personal"]
-  def get_about, do: @portfolio_data["about"]
-  def get_skills, do: @portfolio_data["skills"]
-  def get_experience, do: @portfolio_data["experience"]
-  def get_projects, do: @portfolio_data["projects"]
-  def get_awards, do: @portfolio_data["awards"]
-  def get_volunteer, do: @portfolio_data["volunteer"]
-  def get_education, do: @portfolio_data["education"]
+  def get_personal, do: @portfolio_data[:personal]
+  def get_about, do: @portfolio_data[:about]
+  def get_skills, do: @portfolio_data[:skills]
+  def get_experience, do: @portfolio_data[:experience]
+  def get_projects, do: @portfolio_data[:projects]
+  def get_awards, do: @portfolio_data[:awards]
+  def get_volunteer, do: @portfolio_data[:volunteer]
+  def get_education, do: @portfolio_data[:education]
 
-  def get_featured_projects, do: @portfolio_data["projects"]["featured"]
+  def get_featured_projects, do: @portfolio_data[:projects][:featured]
 
   def get_all_projects,
-    do: @portfolio_data["projects"]["featured"] ++ @portfolio_data["projects"]["all"]
+    do: @portfolio_data[:projects][:featured] ++ @portfolio_data[:projects][:all]
 end
